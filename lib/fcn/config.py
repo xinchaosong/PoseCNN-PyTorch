557cfc5fbacdd13c778c1b81f54b5cd227f0a1c6
Xinchao Song <git@xinchaosong.com>
2023-02-23 17:09:15 -0500

    Update config.py

--- lib/fcn/config.py
+++ lib/fcn/config.py
@@ -374,7 +374,7 @@ def cfg_from_file(filename):
     """Load a config file and merge it into the default options."""
     import yaml
     with open(filename, 'r') as f:
-        yaml_cfg = edict(yaml.load(f))
+        yaml_cfg = edict(yaml.load(f, Loader=yaml.Loader))
 
     _merge_a_into_b(yaml_cfg, __C)
 
@@ -383,5 +383,5 @@ def yaml_from_file(filename):
     """Load a config file and merge it into the default options."""
     import yaml
     with open(filename, 'r') as f:
-        yaml_cfg = edict(yaml.load(f))
+        yaml_cfg = edict(yaml.load(f, Loader=yaml.Loader))
     return yaml_cfg
